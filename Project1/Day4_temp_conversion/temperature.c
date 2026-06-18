804485e5758b34a5315af7f8de6e20b9b4240ae3
Naveen B Nekar <naveenbnekar@gmail.com>
2026-06-18 17:27:18 +0530

    added a program

---
+++ Project1/Day4_temp_conversion/temperature.c
@@ -0,0 +1,18 @@
+//Write a C program to convert temperature between Celsius and Fahrenheit.
+#include<stdio.h>
+int main(){
+    float celsius,fahrenheit;
+    //Read the temperature by user 
+    printf("Enter the temperature in celsius :");
+    scanf(" %f", &celsius);
+
+    //celsius to fahrenheit conversion
+
+    fahrenheit = ((9.0  / 5.0 )* celsius) + 32 ;
+
+    //Displaying the temperature value on the screen
+
+    printf("The temperature value in the fahrenheit is : %f",fahrenheit );
+    return 0;
+
+}
