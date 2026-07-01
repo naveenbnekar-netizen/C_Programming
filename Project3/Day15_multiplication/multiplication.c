e53f433a364a49711384239f6dff1b3a22ea80db
Naveen B Nekar <naveenbnekar@gmail.com>
2026-07-01 20:00:18 +0530

    Added the program

---
+++ Project3/Day15_multiplication/multiplication.c
@@ -0,0 +1,20 @@
+/*Write a C program to generate the multiplication table of a given number.
+The program should accept an integer from the user and display its multiplication table from 1 to 10 in a clear and formatted manner.*/
+#include<stdio.h>
+int main() {
+    int num;
+
+    printf("Enter and integr : ");
+    scanf("%d",&num);
+    printf("==================================\n");
+    printf("Multiplication table of %d\n",num);
+    printf("==================================\n");
+
+
+    for(int i = 1; i<= 10; i++) {
+
+        printf("%d * %d = %d\n",num,i,num*i);
+
+    }
+     printf("==================================\n");
+}
