15f32bb39eb866ca8e5bee6054b633d7143f2485
Naveen B Nekar <naveenbnekar@gmail.com>
2026-07-01 20:02:48 +0530

    added comments

--- Project3/Day15_multiplication/multiplication.c
+++ Project3/Day15_multiplication/multiplication.c
@@ -3,18 +3,20 @@ The program should accept an integer from the user and display its multiplicatio
 #include<stdio.h>
 int main() {
     int num;
+    //Reading value from the user
 
     printf("Enter and integr : ");
     scanf("%d",&num);
+
     printf("==================================\n");
     printf("Multiplication table of %d\n",num);
     printf("==================================\n");
-
-
+//using the for loop to generate the multiplication table
     for(int i = 1; i<= 10; i++) {
 
         printf("%d * %d = %d\n",num,i,num*i);
 
     }
      printf("==================================\n");
+     return 0;
 }
