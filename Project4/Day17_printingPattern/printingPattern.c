44c1c21ab95681f473975a36927d061d1e52d05e
Naveen B Nekar <naveenbnekar@gmail.com>
2026-07-04 08:44:39 +0530

    Aded Program

--- Project4/Day17_printingPattern/printingPattern.c
+++ Project4/Day17_printingPattern/printingPattern.c
@@ -1,21 +1,18 @@
 #include<stdio.h>
-int main () {
+int main() {
     int n;
     printf("Enter the size of the square : ");
-    scanf("%d",&n);
+    scanf("%d", &n);
     //Validate the zero and negative number
     if (n <= 0) {
-
         printf("Invalid number");
-
+        return 1;
     }
     //Prints each row
     for (int i = 1; i <= n; i++) {
         //Prints stars in the column
         for (int j = 1; j <= n; j++) {
-
             printf("*");
-
         }
         printf("\n");
     }
