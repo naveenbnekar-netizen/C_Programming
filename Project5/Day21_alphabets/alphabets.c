cfd653366f81a17a6f9c27b28fdfce4d3f32fa9c
Naveen B Nekar <naveenbnekar@gmail.com>
2026-07-08 21:05:00 +0530

    Added program

---
+++ Project5/Day21_alphabets/alphabets.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main () {
+    char ch = 'A';
+    printf("Uppercase Alphabets are: \n");
+    while (ch <= 'Z') {
+        printf(" %c ", ch);
+        ch ++;
+    }
+
+return 0;
+}
