543846320e86829c5420dd1b9595ff9ddb174b0f
Naveen B Nekar <naveenbnekar@gmail.com>
2026-07-10 21:50:38 +0530

    added Program

---
+++ Project5/Day23_reverse/reverse.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+    int num, reverse = 0, remainder;
+
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    for( ;num != 0; num /= 10) {
+        remainder = num % 10;
+        reverse = reverse * 10 + remainder;
+
+    }
+    printf("The reverse num is : %d",reverse);
+return 0;
+
+}
